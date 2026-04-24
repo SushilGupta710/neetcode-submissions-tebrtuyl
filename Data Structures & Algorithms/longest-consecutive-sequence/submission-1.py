8c56bd62bf77a05f0c561d20306d283afd473fef
Sushil Gupta <52453300+SushilGupta710@users.noreply.github.com>
2026-04-24 12:31:49 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #to store the unique and sequence order value with O(n) TC
+        numSet = set(nums)
+        result=0
+
+        for num in nums:
+            #First here we need to check if num-1 is not exists in set then we need to track the longest sequence
+            #So basically we need to check the start value is not present then track the length
+            if num-1 not in numSet:
+                length=0
+                while (num+length) in numSet:
+                    length+=1
+                result = max(result,length)
+        return result
+
