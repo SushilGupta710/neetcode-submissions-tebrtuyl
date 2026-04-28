daacc019c696b45bc99db9cf72dd57bbcc1b3e63
Sushil Gupta <52453300+SushilGupta710@users.noreply.github.com>
2026-04-28 23:29:42 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        openCloseDict = {"}":"{",")":"(","]":"["}
+        paranStack=[]
+        for c in s:
+            if paranStack and openCloseDict.get(c) == paranStack[-1]:
+                paranStack.pop()
+            else:
+                paranStack.append(c)
+
+        return True if not paranStack else False
